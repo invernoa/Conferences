9e821948311dc9c28323ede8a26764899fc05255
Irina Gorbunova <igorbunova@igorbunova-mac.local>
2019-04-29 01:37:33 +0300

    add hosting

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'invernoa.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'invernoa.pythonanywhere.com', 'cieconf.herokuapp.com']
 
 SITE_ID = 1
 
